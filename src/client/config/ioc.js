35d942b60fd30e3d7d16baa65924c7e5f6e73dde
Tu.Tran <thanhtuit27@gmail.com>
2015-04-09 05:09:27 +0700

    commit to local

--- src/client/config/ioc.js
+++ src/client/config/ioc.js
@@ -4,7 +4,8 @@ define([
 	], function() {
     var iocItems = [
     	{ type: 'ILogger', name:"Default", instanceOf: "share/helper/logging/defaultlogger"},
-    	{ type: 'ILocalStore', instanceOf: "share/helper/storage/sessionLocalStore"}
+    	{ type: 'ILocalStore', instanceOf: "share/helper/storage/sessionLocalStore"},
+    	{ type: 'ICommonDataService', instanceOf: "client/module/common/dataService/commonDataService"}
     ];
     return iocItems;
 });
